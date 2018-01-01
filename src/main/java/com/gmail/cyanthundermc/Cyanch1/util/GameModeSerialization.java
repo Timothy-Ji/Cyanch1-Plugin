a4a4a181a96d5d5cefbccd2f3e78ebe039bcb955
TimothyJi <Timothyji111@gmail.com>
2017-12-31 23:15:06 -0500

    Correctly Staticed.

--- src/main/java/com/gmail/cyanthundermc/Cyanch1/util/GameModeSerialization.java
+++ src/main/java/com/gmail/cyanthundermc/Cyanch1/util/GameModeSerialization.java
@@ -3,11 +3,11 @@ package com.gmail.cyanthundermc.Cyanch1.util;
 import org.bukkit.GameMode;
 
 public class GameModeSerialization {
-    public String getStringFromGameMode(GameMode gameMode) {
+    public static String getStringFromGameMode(GameMode gameMode) {
         return gameMode.name();
     }
 
-    public GameMode getGameModeFromString(String str) {
+    public static GameMode getGameModeFromString(String str) {
         for (GameMode gameMode : GameMode.values()) {
             if (gameMode.name().equalsIgnoreCase(str))
                 return gameMode;
