b50239f3c17a0f6cb4deec9de4c10ca597d84bb1
TimothyJi <Timothyji111@gmail.com>
2018-01-01 13:01:03 -0500

    Cleaner Implementation of ServerWorld and SQLLite Interaction.

--- src/main/java/com/gmail/cyanthundermc/Cyanch1/serverworld/ServerWorld.java
+++ src/main/java/com/gmail/cyanthundermc/Cyanch1/serverworld/ServerWorld.java
@@ -7,14 +7,15 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public enum ServerWorld {
-    CREATIVE("creative", ChatColor.YELLOW, "" + ChatColor.YELLOW + ChatColor.BOLD + "Creative " + ChatColor.GRAY + "> " + ChatColor.RESET),
-    SURVIVAL("world", ChatColor.GREEN, "" + ChatColor.GREEN + ChatColor.BOLD + "Survival " +ChatColor.GREEN + " > " + ChatColor.RESET);
+    CREATIVE("creative", ChatColor.YELLOW, "" + ChatColor.YELLOW + ChatColor.BOLD + "Creative " + ChatColor.GRAY + "> " + ChatColor.RESET, "creative_sw_data"),
+    SURVIVAL("world", ChatColor.GREEN, "" + ChatColor.GREEN + ChatColor.BOLD + "Survival " +ChatColor.GREEN + " > " + ChatColor.RESET, "survival_sw_data");
 
     private String worldName;
     private ChatColor color;
     private String announcementFormat;
+    private String db_table_name;
 
-    ServerWorld(String worldName, ChatColor color, String announcementFormat) {
+    ServerWorld(String worldName, ChatColor color, String announcementFormat, String database_table_name) {
         this.worldName = worldName;
         this.announcementFormat = announcementFormat;
     }
@@ -47,6 +48,10 @@ public enum ServerWorld {
             }
         }
         CyanchPlugin.INSTANCE.getServer().getLogger().info("[Local Announcement - " + this.name() + "] " + message);
+    }
+
+    public String getDb_table_name() {
+        return db_table_name;
     }
 }
 
--- src/main/java/com/gmail/cyanthundermc/Cyanch1/sqllite/SQLLite.java
+++ src/main/java/com/gmail/cyanthundermc/Cyanch1/sqllite/SQLLite.java
@@ -1,35 +1,31 @@
 package com.gmail.cyanthundermc.Cyanch1.sqllite;
 
 import com.gmail.cyanthundermc.Cyanch1.CyanchPlugin;
+import com.gmail.cyanthundermc.Cyanch1.serverworld.ServerWorld;
 
 public class SQLLite {
 
     private static CyanchPlugin plugin = CyanchPlugin.INSTANCE;
     public static String player_database_name = "CyanchS1_PlayerDB";
-    public static String player_db_table_creative_data_name = "creative_server_data";
-    public static String player_db_table_survival_data_name = "survival_server_data";
 
     public static void Init() {
         //Player DB
+        String createStatement = "";
+        for (ServerWorld serverWorld : ServerWorld.values()) {
+            createStatement +=
+                    "CREATE TABLE IF NOT EXISTS player_"  + serverWorld.getDb_table_name() + "(" +
+                    "UUID TEXT NOT NULL," +
+                    "INVENTORY_CONTENTS TEXT," +
+                    "ARMOR_CONTENTS TEXT," +
+                    "OFF_HAND TEXT," +
+                    "LOCATION TEXT," +
+                    "EXPERIENCE FLOAT," +
+                    "GAMEMODE TEXT" +
+                    ");"
+            ;
+    }
         plugin.sqlLib.initializeDatabase(player_database_name,
-                "CREATE TABLE IF NOT EXISTS " + player_db_table_creative_data_name + "(" +
-                        "UUID TEXT NOT NULL," +
-                        "INVENTORY_CONTENTS TEXT," +
-                        "ARMOR_CONTENTS TEXT," +
-                        "OFF_HAND TEXT," +
-                        "LOCATION TEXT," +
-                        "EXPERIENCE FLOAT," +
-                        "GAMEMODE TEXT" +
-                        ");" +
-                        "CREATE TABLE IF NOT EXISTS " + player_db_table_survival_data_name + "(" +
-                        "UUID TEXT NOT NULL," +
-                        "INVENTORY_CONTENTS TEXT," +
-                        "ARMOR_CONTENTS TEXT," +
-                        "OFF_HAND TEXT" +
-                        "LOCATION TEXT," +
-                        "EXPERIENCE FLOAT," +
-                        "GAMEMODE TEXT" +
-                        ")"
+                createStatement
         );
     }
 }
